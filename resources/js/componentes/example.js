d4fe2f178cc8f0595f059863e02ac97a4600836a
Italo Malta <italoximendes@gmail.com>
2023-06-08 14:52:22 -0300

    adicionando React ao projeto

---
+++ resources/js/componentes/example.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Example/>,document.getElementById('cadastro_usuario'))
+
+function Example(){
+    return(
+        <p>Hola react</p>
+    )
+}
+
+export default Example;
